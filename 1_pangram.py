ee3c6a7d9d726ed6c0e2f6c3e991341835142acf
harshala jagtap <harshalajagtap1619@gmail.com>
2022-06-01 13:29:15 +0530

    is_pangram

--- 1_pangram.py
+++ 1_pangram.py
@@ -1,2 +1,22 @@
-def is_pangram():
-    
+from email import charset
+
+
+def is_pangram(str1):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in alphabet:
+       if char not in str1.lower():
+           return False
+      
+    return True
+
+inpt_str= "The quick brown fox jumps over the lazy dog"
+
+if(is_pangram(inpt_str)== True):
+    print("given string is pangram")
+else:
+    print("given string is not pangram")
+
+
+
+
+
